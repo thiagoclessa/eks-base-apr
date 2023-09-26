d01c8119efcbc4d3c405aa76d826267319232a59
thiagoclessa <80272327+thiagoclessa@users.noreply.github.com>
2023-09-26 11:18:24 -0300

    teste5

--- main.tf
+++ main.tf
@@ -61,7 +61,6 @@ module "irsa-ebs-csi" {
 resource "aws_eks_addon" "ebs-csi" {
   cluster_name             = local.config.cluster_name
   addon_name               = "aws-ebs-csi-driver"
-  addon_version            = "v1.9.0-eksbuild.1"
   service_account_role_arn = module.irsa-ebs-csi.iam_role_arn
   tags = {
     "eks_addon" = "ebs-csi"

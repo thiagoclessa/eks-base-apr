aea4f98575bfc8e3d37d5371f81952a7ec4be56e
thiagoclessa <80272327+thiagoclessa@users.noreply.github.com>
2023-09-26 13:28:46 -0300

    teste 7

--- main.tf
+++ main.tf
@@ -1,12 +1,3 @@
-data "aws_iam_policy" "ebs_csi_policy" {
-  arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
-}
-
-data "aws_eks_cluster" "eks" {
-  name = local.config.cluster_name
-  depends_on = [module.cluster]
-}
-
 module "cluster" {
   source  = "gitlab.com/vkpr/terraform-aws-eks/aws"
   version = "~> 1.3.0"
@@ -47,12 +38,34 @@ module "cluster" {
 #  depends_on = [module.cluster]
 #}
 
+data "aws_eks_cluster" "cluster" {
+  name = local.config.cluster_name
+  depends_on = [module.cluster]  
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = local.config.cluster_name
+  depends_on = [module.cluster]  
+}
+
+data "tls_certificate" "cert" {
+  url = data.aws_eks_cluster.cluster.identity[0].oidc[0].issuer
+  depends_on = [module.cluster]  
+}
+
+resource "aws_iam_openid_connect_provider" "openid_connect" {
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [data.tls_certificate.cert.certificates.0.sha1_fingerprint]
+  url             = data.aws_eks_cluster.cluster.identity[0].oidc[0].issuer
+  depends_on = [module.cluster]  
+}
+
 module "irsa-ebs-csi" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
   version = "4.7.0"
 
   create_role                   = true
-  role_name                     = "AmazonEKSTFEBSCSIRole-${local.config.cluster_name}"
+  role_name                     = "AmazonEKSTFEBSCSIRole-$local.config.cluster_name"
   provider_url                  = replace(data.aws_eks_cluster.eks.identity.0.oidc.0.issuer, "https://", "")
   role_policy_arns              = [data.aws_iam_policy.ebs_csi_policy.arn]
   oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
@@ -62,10 +75,9 @@ resource "aws_eks_addon" "ebs-csi" {
   cluster_name             = local.config.cluster_name
   addon_name               = "aws-ebs-csi-driver"
   service_account_role_arn = module.irsa-ebs-csi.iam_role_arn
-  resolve_conflicts_on_create = "OVERWRITE"
   tags = {
     "eks_addon" = "ebs-csi"
     "terraform" = "true"
+    "origem"    = "platform"
   }
-  depends_on = [module.cluster]
 }

829751664728d817a689fbfea21c755a3ea11f37
thiagoclessa <80272327+thiagoclessa@users.noreply.github.com>
2023-09-26 10:13:35 -0300

    teste remova eks acoount

--- main.tf
+++ main.tf
@@ -21,17 +21,17 @@ module "cluster" {
   aws_availability_zones    = try(local.config.aws_availability_zones, [""])
 }
 
-module "kubernetes" {
-  source  = "gitlab.com/vkpr/terraform-kubernetes-rbac/kubernetes"
-  version = "~> 1.1.0"
+#module "kubernetes" {
+#  source  = "gitlab.com/vkpr/terraform-kubernetes-rbac/kubernetes"
+#  version = "~> 1.1.0"
 
-  users_list             = local.config.users_list
-  cluster_endpoint       = module.cluster.cluster_endpoint
-  cluster_ca_certificate = module.cluster.cluster_certificate_authority_data
-  cluster_access_token   = module.cluster.kubeconfig_token
-
-  depends_on = [module.cluster]
-}
+#  users_list             = local.config.users_list
+#  cluster_endpoint       = module.cluster.cluster_endpoint
+#  cluster_ca_certificate = module.cluster.cluster_certificate_authority_data
+#  cluster_access_token   = module.cluster.kubeconfig_token
+#
+#  depends_on = [module.cluster]
+#}
 
 module "kubeconfig" {
   source  = "gitlab.com/vkpr/terraform-kubernetes-kubeconfig/kubernetes"
@@ -43,7 +43,7 @@ module "kubeconfig" {
   cluster_ca_certificate = module.cluster.cluster_certificate_authority_data
   cluster_access_token   = module.kubernetes.secrets_access_tokens
 
-  depends_on = [module.kubernetes]
+  depends_on = [module.cluster]
 }
 
 module "irsa-ebs-csi" {

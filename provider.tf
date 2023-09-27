ee73403223fa8f28bd6b882caed8a094e8116889
thiagoclessa <80272327+thiagoclessa@users.noreply.github.com>
2023-09-27 11:54:14 -0300

    add provider

--- provider.tf
+++ provider.tf
@@ -1,14 +1,3 @@
-terraform {
-  required_version = ">= 0.13"
-
-  required_providers {
-    kubectl = {
-      source  = "gavinbunney/kubectl"
-      version = ">= 1.7.0"
-    }
-  }
-}
-
 provider "aws" {
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
